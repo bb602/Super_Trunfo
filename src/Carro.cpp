ea5d42fddc16d9b8c391b046062743c5b80fb940
Matheus Dias <54779861+diasmatheus05@users.noreply.github.com>
2019-11-26 01:25:43 -0300

    Add files via upload

--- src/Carro.cpp
+++ src/Carro.cpp
@@ -2,23 +2,23 @@
 #include <iostream>
 #include <string>
 
-std::string Carro::get_cilindradas(){
+int Carro::get_cilindradas(){
 	return cilindradas;
 }
 
-std::string Carro::get_potencia(){
+int Carro::get_potencia(){
 	return potencia;
 }
 
-std::string Carro::get_velocidade(){
+int Carro::get_velocidade(){
 	return velocidade;
 }
 
-std::string Carro::get_peso(){
+int Carro::get_peso(){
 	return peso;
 }
 
-std::string Carro::get_comprimento(){
+float Carro::get_comprimento(){
 	return comprimento;
 }
 
@@ -27,29 +27,29 @@ void Carro::imprime_carta(){
 	std::cout << "Nome: " << get_nome() << std::endl;
 	std::cout << "Tipo: " << get_tipo() << std::endl << std::endl;
 	std::cout << "1 Cilindradas: " << get_cilindradas() << " ccm" << std::endl;
-	std::cout << "2 Potencia: " << get_potencia() << " cv" << std::endl;
+	std::cout << "2 Potência: " << get_potencia() << " cv" << std::endl;
 	std::cout << "3 Velocidade: " << get_velocidade() << " km/h" << std::endl;
 	std::cout << "4 Peso: " << get_peso() << " kg" << std::endl;
 	std::cout << "5 Comprimento: " << get_comprimento() << " m" << std::endl;
 	std::cout << "#########################" << std::endl << std::endl;
 }
 
-void Carro::set_cilindradas(std::string cilindradas){
+void Carro::set_cilindradas(int cilindradas){
 	this-> cilindradas = cilindradas;
 }
 
-void Carro::set_potencia(std::string potencia){
+void Carro::set_potencia(int potencia){
 	this-> potencia = potencia;
 }
 
-void Carro::set_velocidade(std::string velocidade){
+void Carro::set_velocidade(int velocidade){
 	this-> velocidade = velocidade;
 }
 
-void Carro::set_peso(std::string peso){
+void Carro::set_peso(int peso){
 	this-> peso = peso;
 }
 
-void Carro::set_comprimento(std::string comprimento){
+void Carro::set_comprimento(float comprimento){
 	this-> comprimento = comprimento;
 }
