6de7d0a2e47125fa0891e4444b0604d1223ba48e
Matheus Dias <54779861+diasmatheus05@users.noreply.github.com>
2019-11-26 01:23:01 -0300

    Add files via upload

--- src/Aviao.cpp
+++ src/Aviao.cpp
@@ -2,24 +2,27 @@
 #include <iostream>
 #include <string>
 
-std::string Aviao::get_peso(){
+int Aviao::get_peso(){
 	return peso;
 }
 
-std::string Aviao::get_velocidade(){
+int Aviao::get_velocidade(){
 	return velocidade;
 }
 
-std::string Aviao::get_altitude_voo(){
+int Aviao::get_altitude_voo(){
 	return altitude_voo;
 }
 
-std::string Aviao::get_comprimento(){
+float Aviao::get_comprimento(){
 	return comprimento;
 }
 
-std::string Aviao::get_altura(){
+float Aviao::get_altura(){
 	return altura;
+}
+bool Aviao::get_super_trunfo(){
+	return super_trunfo;
 }
 
 void Aviao::imprime_carta (){
@@ -28,28 +31,27 @@ void Aviao::imprime_carta (){
 	std::cout << "Tipo: " << get_tipo() << std::endl << std::endl;
 	std::cout << "1 Peso: " << get_peso() << " kg" << std::endl;
 	std::cout << "2 Velocidade: " << get_velocidade() << " km/h" << std::endl;
-	std::cout << "3 Altitude Voo: " << get_altitude_voo() << " m" << std::endl;
+	std::cout << "3 Altitude Voo: " << get_altitude_voo() << " pes" << std::endl;
 	std::cout << "4 Comprimento: " << get_comprimento() << " m" << std::endl;
 	std::cout << "5 Altura: " << get_altura() << " m" << std::endl;
 	std::cout << "#########################" << std::endl << std::endl;
 }
 
-void Aviao::set_peso(std::string peso){
+void Aviao::set_peso(int peso){
 	this-> peso = peso;
 }
-
-void Aviao::set_velocidade(std::string velocidade){
+void Aviao::set_velocidade(int velocidade){
 	this-> velocidade = velocidade;
 }
-
-void Aviao::set_altitude_voo(std::string altitude_voo){
+void Aviao::set_altitude_voo(int altitude_voo){
 	this-> altitude_voo = altitude_voo;
 }
-
-void Aviao::set_comprimento(std::string comprimento){
+void Aviao::set_comprimento(float comprimento){
 	this-> comprimento = comprimento;
 }
-
-void Aviao::set_altura(std::string altura){
+void Aviao::set_altura(float altura){
 	this-> altura = altura;
+}
+void Aviao::set_super_trunfo(bool super_trunfo){
+	this-> super_trunfo = super_trunfo;
 }
