5513ef6ac150f8e856c6ccba3e306b4615ee7f74
RodBraz <55855200+RodBraz@users.noreply.github.com>
2019-12-03 15:14:21 -0300

    Add files via upload

--- program/main.cpp
+++ program/main.cpp
@@ -5,7 +5,6 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
-#include <stdexcept>
 
 #include "Aviao.h"
 #include "Carro.h"
@@ -43,79 +42,75 @@ void jogarh();
 int main() {
 
     for(;;) {
-        try{    
-            int x = 0;
-            std::cout << "Escolha uma opcao: " << std::endl;
-            std::cout << "1 - Baralho Aviao" << std::endl;
-            std::cout << "2 - Baralho Carro" << std::endl;
-            std::cout << "3 - Baralho Dinossauro" << std::endl;
-            std::cout << "4 - Baralho Heroi" << std::endl;
-            std::cout << "5 - Regras" << std::endl << std::endl;
-            std::cout << "OPCAO: ";
-            std::cin >> x;
+        int x = 0;
+        std::cout << "Escolha uma opcao: " << std::endl;
+        std::cout << "1 - Baralho Aviao" << std::endl;
+        std::cout << "2 - Baralho Carro" << std::endl;
+        std::cout << "3 - Baralho Dinossauro" << std::endl;
+        std::cout << "4 - Baralho Heroi" << std::endl;
+        std::cout << "5 - Regras" << std::endl << std::endl;
+        std::cout << "OPCAO: ";
+        std::cin >> x;
 
-            switch(x){
-            case 1:
-                std::cout << "Super Trunfo Aviao!" << std::endl;
+        switch(x){
+        case 1:
+            std::cout << "Super Trunfo Aviao!" << std::endl;
 
-                aviao.cartas_aviao();
-                aviao.embaralhar_cartas();
-                aviao.inicializar_pilhas();
-                aviao.jogar();
+            aviao.cartas_aviao();
+            aviao.embaralhar_cartas();
+            aviao.inicializar_pilhas();
+            aviao.jogar();
 
-                break;
-            case 2:
-                std::cout << "Super Trunfo Carro!" << std::endl;
-                
-                carro.cartas_carro();
-                carro.embaralhar_cartasC();
-                carro.inicializar_pilhas_c();
-                carro.jogarc();
+            break;
+        case 2:
+            std::cout << "Super Trunfo Carro!" << std::endl;
+            
+            carro.cartas_carro();
+            carro.embaralhar_cartasC();
+            carro.inicializar_pilhas_c();
+            carro.jogarc();
 
-                break;
-            case 3:
-                std::cout << "Super Trunfo Dinossauro!" << std::endl;
-                
-                dinossauro.cartas_dinossauro();
-                dinossauro.embaralhar_cartasD();
-                dinossauro.inicializar_pilhas_d();
-                dinossauro.jogard();
+            break;
+        case 3:
+            std::cout << "Super Trunfo Dinossauro!" << std::endl;
+            
+            dinossauro.cartas_dinossauro();
+            dinossauro.embaralhar_cartasD();
+            dinossauro.inicializar_pilhas_d();
+            dinossauro.jogard();
 
-                break;
-            case 4:
-                std::cout << "Super Trunfo Heroi!" << std::endl;
-                
-                heroi.cartas_heroi();
-                heroi.embaralhar_cartasH();
-                heroi.inicializar_pilhas_h();
-                heroi.jogarh();
+            break;
+        case 4:
+            std::cout << "Super Trunfo Heroi!" << std::endl;
+            
+            heroi.cartas_heroi();
+            heroi.embaralhar_cartasH();
+            heroi.inicializar_pilhas_h();
+            heroi.jogarh();
 
-                break;
-            case 5:
-                std::cout << "Super Trunfo Regras!" << std::endl;
-                std::cout << "1. Observe em cada carta qual valor vence em cada rodada: o maior ou o menor" << std::endl;
-                std::cout << "2. Para iniciar, escolha entre as informações da sua carta, aquela que você julga" << std::endl;
-                std::cout << "   ter o valor capaz de vencer as cartas dos seus adversários." << std::endl;
-                std::cout << "3. Se você vencer - as cartas dos outros jogadores irão para trás do seu monte de" << std::endl;
-                std::cout << "   cartas e você continua escolhendo a informação da sua próxima carta." << std::endl;
-                std::cout << "4. Se outro jogador vencer - as cartas irão para trás do monte de cartas dele e a" << std::endl; 
-                std::cout << "   vez de escolher passa para ele." << std::endl;
-                std::cout << "5. Em caso de empate - as cartas irão para o monte e as próximas cartas - sua e dos" << std::endl;
-                std::cout << "   seus adversários - aparecem. Uma nova disputa é feita sendo que o jogador que " << std::endl; 
-                std::cout << "   escolheu as cartas que empataram deve escolher novamente." << std::endl;
-                std::cout << "6. Existe entre as cartas uma carta SUPER TRUNFO. Esta carta vence todas as cartas do" << std::endl; 
-                std::cout << "   baralho independentemente do valor de suas características. Ela perde apenas para as" << std::endl;
-                std::cout << "   cartas que tenham a letra A (1A, 2A, 3A, etc), marcado na parte superior de cada uma." << std::endl;
-                std::cout << "7. Quando a carta SUPER TRUNFO aparecer, a comparação será automática com as cartas dos adversários." << std::endl;
-                std::cout << "8. O jogo termina quando um dos jogadores ganhar todas as cartas do baralho." << std::endl << std::endl;
-                
-                break;
-            default:
-                std::cout << "Opcao invalida!" << std::endl;
-                break;
-            }
-        } catch (std::exception &e) {
+            break;
+        case 5:
+            std::cout << "Super Trunfo Regras!" << std::endl;
+            std::cout << "1. Observe em cada carta qual valor vence em cada rodada: o maior ou o menor" << std::endl;
+            std::cout << "2. Para iniciar, escolha entre as informações da sua carta, aquela que você julga" << std::endl;
+            std::cout << "   ter o valor capaz de vencer as cartas dos seus adversários." << std::endl;
+            std::cout << "3. Se você vencer - as cartas dos outros jogadores irão para trás do seu monte de" << std::endl;
+            std::cout << "   cartas e você continua escolhendo a informação da sua próxima carta." << std::endl;
+            std::cout << "4. Se outro jogador vencer - as cartas irão para trás do monte de cartas dele e a" << std::endl; 
+            std::cout << "   vez de escolher passa para ele." << std::endl;
+            std::cout << "5. Em caso de empate - as cartas irão para o monte e as próximas cartas - sua e dos" << std::endl;
+            std::cout << "   seus adversários - aparecem. Uma nova disputa é feita sendo que o jogador que " << std::endl; 
+            std::cout << "   escolheu as cartas que empataram deve escolher novamente." << std::endl;
+            std::cout << "6. Existe entre as cartas uma carta SUPER TRUNFO. Esta carta vence todas as cartas do" << std::endl; 
+            std::cout << "   baralho independentemente do valor de suas características. Ela perde apenas para as" << std::endl;
+            std::cout << "   cartas que tenham a letra A (1A, 2A, 3A, etc), marcado na parte superior de cada uma." << std::endl;
+            std::cout << "7. Quando a carta SUPER TRUNFO aparecer, a comparação será automática com as cartas dos adversários." << std::endl;
+            std::cout << "8. O jogo termina quando um dos jogadores ganhar todas as cartas do baralho." << std::endl << std::endl;
+            
+            break;
+        default:
             std::cout << "Opcao invalida!" << std::endl;
+            break;
         }
     }
     
